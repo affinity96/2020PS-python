86e3ce853c782052d7b9cf0bff946fa797888822
affinity96 <affinity96@gmail.com>
2020-02-11 14:31:19 +0900

    Create 소수찾기.py

---
+++ 완전탐색/소수찾기.py
@@ -0,0 +1,24 @@
+import itertools
+def isItSosu(number):
+    
+    if number < 2 : return False
+    if number == 2 : return True
+    if number % 2 == 0 : return False
+    #square = round(number**0.5) + 1
+    for i in range(3, number, 2) : 
+        if number % i == 0 : return False
+    return True
+
+def solution(numbers):
+    answer = 0
+    for length,_ in enumerate(numbers):
+        numbers_list = list(set(map(''.join, itertools.permutations(numbers,length+1))))
+        
+        for number in numbers_list:
+            
+            if number[0]!='0' : 
+                if isItSosu(int(number)):
+
+                    answer+=1
+
+    return answer
